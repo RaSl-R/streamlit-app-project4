d09d474acfdeb9841a8efe7ab8dd480093d333c2
RaSl_R <rasl.r@seznam.cz>
2025-08-24 13:03:09 +0200

    rsl3

--- utils/db.py
+++ utils/db.py
@@ -2,13 +2,10 @@ import streamlit as st
 import psycopg2
 from sqlalchemy import create_engine
 
-conn = psycopg2.connect(
-    host=st.secrets["database"]["host"],
-    port=st.secrets["database"]["port"],
-    user=st.secrets["database"]["user"],
-    password=st.secrets["database"]["password"],
-    database=st.secrets["database"]["database"]
-)
+DB_USER = st.secrets["DB_USER"]
+DB_PASSWORD = st.secrets["DB_PASSWORD"]
+DB_HOST = st.secrets["DB_HOST"]
+DB_NAME = st.secrets["DB_NAME"]
 
 @st.cache_resource
 def get_engine():
